3aae131691e1ff78ab52e8d6144123c47f3038ab
sibil.kurian@gmail.com <sibil.kurian@gmail.com>
2020-08-31 23:45:33 -0400

    model

---
+++ src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,24 @@
+package com.example.demo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table
+public class User {
+    @Id
+    private int id;
+    private String profession;
+    private String name;
+    private int age;
+}
