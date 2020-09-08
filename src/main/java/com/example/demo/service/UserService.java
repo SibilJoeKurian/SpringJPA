c5aef9e6340ed554f4a951202915e4bb0929dfe6
sibil.kurian@gmail.com <sibil.kurian@gmail.com>
2020-09-08 10:06:43 -0400

    Added a word file documentation

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -1,2 +1,17 @@
-package com.example.demo.service;public class UserService {
+package com.example.demo.service;
+
+import com.example.demo.dao.UserRepository;
+import com.example.demo.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository repository;
+    public List<User> getUsers(){
+        return repository.findAll();
+    }
 }
